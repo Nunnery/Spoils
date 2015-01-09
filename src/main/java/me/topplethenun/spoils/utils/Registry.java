b9b6154945b0a04254c1a4140dd82c5fc4b7d532
Richard Harrah <topplethenunnery@gmail.com>
2015-01-08 19:31:22 -0500

    adding Registry generic

---
+++ src/main/java/me/topplethenun/spoils/utils/Registry.java
@@ -0,0 +1,28 @@
+package me.topplethenun.spoils.utils;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Registry<T> {
+
+    private Set<T> registeredSet;
+
+    public Registry() {
+        this.registeredSet = new HashSet<>();
+    }
+
+    public Registry<T> register(T item) {
+        this.registeredSet.add(item);
+        return this;
+    }
+
+    public Registry<T> unregister(T item) {
+        this.registeredSet.remove(item);
+        return this;
+    }
+
+    public Set<T> getRegisteredItems() {
+        return new HashSet<>(registeredSet);
+    }
+
+}
