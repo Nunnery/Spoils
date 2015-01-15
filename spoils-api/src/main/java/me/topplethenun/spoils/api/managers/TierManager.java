865d3ceb15a996f4f89803a7d253cd81c1e55420
Richard Harrah <topplethenunnery@gmail.com>
2015-01-15 09:45:00 -0500

    adding TierManager interface

---
+++ spoils-api/src/main/java/me/topplethenun/spoils/api/managers/TierManager.java
@@ -0,0 +1,8 @@
+package me.topplethenun.spoils.api.managers;
+
+import me.topplethenun.spoils.api.tiers.Tier;
+import me.topplethenun.spoils.common.managers.Manager;
+
+public interface TierManager extends Manager<Tier> {
+
+}
