702b8f81618078b6a3f72c47b0e0b6bd4e563952
Richard Harrah <topplethenunnery@gmail.com>
2015-03-03 20:23:40 -0500

    adding MessageUtils

---
+++ spoils-common/src/main/java/com/tealcube/minecraft/spigot/spoils/common/utils/MessageUtils.java
@@ -0,0 +1,34 @@
+/*
+ * This file is part of spoils-common, licensed under the ISC License.
+ *
+ * Copyright (c) 2014 Richard Harrah
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any purpose with or without fee is hereby granted,
+ * provided that the above copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+ * INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF
+ * THIS SOFTWARE.
+ */
+package com.tealcube.minecraft.spigot.spoils.common.utils;
+
+import org.bukkit.command.CommandSender;
+
+public final class MessageUtils {
+
+    private MessageUtils() {
+        // do nothing
+    }
+
+    public static void sendMessage(CommandSender receiver, String message) {
+        sendMessage(receiver, message, new String[][]{{}});
+    }
+
+    public static void sendMessage(CommandSender receiver, String message, String[][] strings) {
+        String msg = TextUtils.color(TextUtils.args(message, strings));
+        receiver.sendMessage(msg);
+    }
+
+}
