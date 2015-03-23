afbc796cb2d906e0fa45a0826d5f4f73b1ebddac
Richard Harrah <topplethenunnery@gmail.com>
2015-03-22 23:53:47 -0400

    adding IdentificationListener and updating license headers

---
+++ spoils-identification/src/main/java/com/tealcube/minecraft/bukkit/spoils/identification/IdentificationListener.java
@@ -0,0 +1,73 @@
+/*
+ * This file is part of spoils-identification, licensed under the ISC License.
+ *
+ * Copyright (c) 2014 Richard Harrah
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any purpose with or without fee is hereby granted,
+ * provided that the above copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+ * INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF
+ * THIS SOFTWARE.
+ */
+package com.tealcube.minecraft.bukkit.spoils.identification;
+
+import com.tealcube.minecraft.bukkit.hilt.HiltItemStack;
+import com.tealcube.minecraft.spigot.spoils.common.utils.MessageUtils;
+import com.tealcube.minecraft.spigot.spoils.common.utils.TextUtils;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Event;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.ClickType;
+import org.bukkit.event.inventory.InventoryClickEvent;
+
+public class IdentificationListener implements Listener {
+
+    private SpoilsIdentification identification;
+
+    public IdentificationListener(SpoilsIdentification identification) {
+        this.identification = identification;
+    }
+
+    @EventHandler(priority = EventPriority.LOWEST)
+    public void onInventoryClick(InventoryClickEvent event) {
+        if (event.getCurrentItem() == null || event.getCursor() == null
+                || event.getCurrentItem().getType() == Material.AIR || event.getCursor().getType() == Material.AIR ||
+                !(event.getWhoClicked() instanceof Player) || event.getClick() != ClickType.RIGHT) {
+            return;
+        }
+        Player player = (Player) event.getWhoClicked();
+        HiltItemStack currentItem = new HiltItemStack(event.getCurrentItem());
+        HiltItemStack cursor = new HiltItemStack(event.getCursor());
+
+        if (cursor.getName() == null) {
+            return;
+        }
+
+        if (!cursor.getName().equals(TextUtils.color(identification.getSettings().getString("config.identity-tome.name")))) {
+            return;
+        }
+        if (!currentItem.getName().equals(TextUtils.color(identification.getSettings().getString("config.unidentified-item.name")))) {
+            return;
+        }
+
+        Material m = currentItem.getType();
+        currentItem = identification.getSpoilsPlugin().getNewItemBuilder().withMaterial(m).withTier(identification.getTierIdentificationChance())
+                .build();
+
+        MessageUtils.sendMessage(event.getWhoClicked(), "<green>You successfully identified your item!");
+
+        event.setCurrentItem(currentItem);
+        cursor.setAmount(cursor.getAmount() - 1);
+        event.setCursor(cursor.getAmount() == 0 ? null : cursor);
+        event.setCancelled(true);
+        event.setResult(Event.Result.DENY);
+        player.updateInventory();
+    }
+
+}
--- spoils-identification/src/main/java/com/tealcube/minecraft/bukkit/spoils/identification/SpoilsIdentification.java
+++ spoils-identification/src/main/java/com/tealcube/minecraft/bukkit/spoils/identification/SpoilsIdentification.java
@@ -1,3 +1,17 @@
+/*
+ * This file is part of spoils-identification, licensed under the ISC License.
+ *
+ * Copyright (c) 2014 Richard Harrah
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any purpose with or without fee is hereby granted,
+ * provided that the above copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+ * INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF
+ * THIS SOFTWARE.
+ */
 package com.tealcube.minecraft.bukkit.spoils.identification;
 
 import com.tealcube.minecraft.bukkit.config.MasterConfiguration;
@@ -6,10 +20,12 @@ import com.tealcube.minecraft.bukkit.config.VersionedSmartYamlConfiguration;
 import com.tealcube.minecraft.spigot.spoils.api.SpoilsPlugin;
 import com.tealcube.minecraft.spigot.spoils.api.tiers.Tier;
 import com.tealcube.minecraft.spigot.spoils.common.io.Debugger;
+import com.tealcube.minecraft.spigot.spoils.common.math.SpoilsRandom;
 import com.tealcube.minecraft.spigot.spoils.common.utils.MessageUtils;
 import org.bukkit.Material;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.event.HandlerList;
 import org.bukkit.plugin.java.JavaPlugin;
 import se.ranzdo.bukkit.methodcommand.Arg;
 import se.ranzdo.bukkit.methodcommand.Command;
@@ -17,6 +33,8 @@ import se.ranzdo.bukkit.methodcommand.FlagArg;
 import se.ranzdo.bukkit.methodcommand.Flags;
 
 import java.io.File;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.logging.Level;
 
 public class SpoilsIdentification extends JavaPlugin {
@@ -24,9 +42,11 @@ public class SpoilsIdentification extends JavaPlugin {
     private MasterConfiguration settings;
     private Debugger debugger;
     private SpoilsPlugin spoilsPlugin;
+    private SpoilsRandom random;
 
     @Override
     public void onEnable() {
+        random = new SpoilsRandom();
         this.spoilsPlugin = (SpoilsPlugin) getServer().getPluginManager().getPlugin("spoils-core");
         this.debugger = new Debugger(new File(getDataFolder(), "debug.log"));
 
@@ -65,7 +85,14 @@ public class SpoilsIdentification extends JavaPlugin {
             }
         }, 20L * 5);
 
+        getServer().getPluginManager().registerEvents(new IdentificationListener(this), this);
+
         debug("Enabling " + getDescription().getName() + " v" + getDescription().getVersion());
+    }
+
+    @Override
+    public void onDisable() {
+        HandlerList.unregisterAll(this);
     }
 
     /**
@@ -94,6 +121,23 @@ public class SpoilsIdentification extends JavaPlugin {
      */
     public MasterConfiguration getSettings() {
         return settings;
+    }
+
+    public Tier getTierIdentificationChance() {
+        double amount = random.nextDouble() * 100;
+        double curAmount = 0D;
+        Set<Tier> tiers = new HashSet<>(spoilsPlugin.getTierManager().getManaged());
+        for (Tier t : tiers) {
+            curAmount += (double) t.getTraitValue(IdentificationTierTrait.IDENTIFICATION_CHANCE);
+            if (curAmount >= amount) {
+                return t;
+            }
+        }
+        return null;
+    }
+
+    public SpoilsPlugin getSpoilsPlugin() {
+        return spoilsPlugin;
     }
 
     public IdentityTome getNewIdentityTome() {
