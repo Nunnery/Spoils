73dcb0ab5aa943e70e0ea8bb7869225c8cc8ffa5
Richard Harrah <topplethenunnery@gmail.com>
2015-05-23 22:55:55 -0400

    cleaning tier loading

--- spoils-core/src/main/java/com/tealcube/minecraft/spigot/spoils/loaders/TierLoaderImpl.java
+++ spoils-core/src/main/java/com/tealcube/minecraft/spigot/spoils/loaders/TierLoaderImpl.java
@@ -14,23 +14,16 @@
  */
 package com.tealcube.minecraft.spigot.spoils.loaders;
 
-import com.google.common.base.Optional;
-import com.tealcube.minecraft.spigot.spoils.SpoilsPluginImpl;
-import com.tealcube.minecraft.spigot.spoils.api.items.ItemGroup;
 import com.tealcube.minecraft.spigot.spoils.api.loaders.TierLoader;
 import com.tealcube.minecraft.spigot.spoils.api.tiers.StandardTierTrait;
 import com.tealcube.minecraft.spigot.spoils.api.tiers.Tier;
 import com.tealcube.minecraft.spigot.spoils.api.tiers.TierTrait;
-import com.tealcube.minecraft.spigot.spoils.common.enchantments.LeveledEnchantment;
-import com.tealcube.minecraft.spigot.spoils.common.utils.TextUtils;
 import com.tealcube.minecraft.spigot.spoils.tiers.TierImpl;
 import org.apache.commons.lang.Validate;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.YamlConfiguration;
 
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 public class TierLoaderImpl implements TierLoader {
@@ -55,70 +48,10 @@ public class TierLoaderImpl implements TierLoader {
             ConfigurationSection cs = configuration.getConfigurationSection(key);
             TierImpl tier = new TierImpl(key);
             for (StandardTierTrait trait : StandardTierTrait.values()) {
-                switch (trait) {
-                    case DISPLAY_NAME:
-                        tier.setTraitValue(trait, cs.getString(trait.key()));
-                        break;
-                    case PRIMARY_COLOR:
-                        tier.setTraitValue(trait, TextUtils.convertTag(cs.getString(trait.key())));
-                        break;
-                    case SECONDARY_COLOR:
-                        tier.setTraitValue(trait, TextUtils.convertTag(cs.getString(trait.key())));
-                        break;
-                    case FLAVOR_TEXT:
-                        tier.setTraitValue(trait, cs.getStringList(trait.key()));
-                        break;
-                    case BASE_ENCHANTMENTS:
-                        Set<LeveledEnchantment> baseEnch = new HashSet<>();
-                        for (String s : cs.getStringList(trait.key())) {
-                            baseEnch.add(LeveledEnchantment.fromString(s));
-                        }
-                        tier.setTraitValue(trait, baseEnch);
-                        break;
-                    case BONUS_ENCHANTMENTS:
-                        Set<LeveledEnchantment> bonusEnch = new HashSet<>();
-                        for (String s : cs.getStringList(trait.key())) {
-                            bonusEnch.add(LeveledEnchantment.fromString(s));
-                        }
-                        tier.setTraitValue(trait, bonusEnch);
-                        break;
-                    case MINIMUM_BONUS_ENCHANTMENTS:
-                        tier.setTraitValue(trait, cs.getInt(trait.key()));
-                        break;
-                    case MAXIMUM_BONUS_ENCHANTMENTS:
-                        tier.setTraitValue(trait, cs.getInt(trait.key()));
-                        break;
-                    case ALLOWED_ITEM_GROUPS:
-                        List<ItemGroup> allowedItemGroups = new ArrayList<>();
-                        for (String s : cs.getStringList(trait.key())) {
-                            Optional<ItemGroup> itemGroupOptional = SpoilsPluginImpl.getInstance()
-                                    .getItemGroupManager().find(s);
-                            if (!itemGroupOptional.isPresent()) {
-                                continue;
-                            }
-                            ItemGroup itemGroup = itemGroupOptional.get();
-                            allowedItemGroups.add(!itemGroup.isInverse() ? itemGroup : itemGroup.inverse());
-                        }
-                        tier.setTraitValue(trait, allowedItemGroups);
-                        break;
-                    case DISALLOWED_ITEM_GROUPS:
-                        List<ItemGroup> disallowedItemGroups = new ArrayList<>();
-                        for (String s : cs.getStringList(trait.key())) {
-                            Optional<ItemGroup> itemGroupOptional = SpoilsPluginImpl.getInstance()
-                                    .getItemGroupManager().find(s);
-                            if (!itemGroupOptional.isPresent()) {
-                                continue;
-                            }
-                            ItemGroup itemGroup = itemGroupOptional.get();
-                            disallowedItemGroups.add(itemGroup.isInverse() ? itemGroup : itemGroup.inverse());
-                        }
-                        tier.setTraitValue(trait, disallowedItemGroups);
-                        break;
-                }
+                tier.setTraitValue(trait, trait.load(cs));
             }
             for (TierTrait trait : traits) {
-                Object value = cs.get(trait.key(), trait.defaultValue());
-                tier.setTraitValue(trait, value);
+                tier.setTraitValue(trait, trait.load(cs));
             }
             tiers.add(tier);
         }
